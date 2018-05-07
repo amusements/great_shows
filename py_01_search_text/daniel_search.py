70e0dd5b86c7bc7ad8a28a628e08d7047dd9b907
Wei Lin Lee <gtr8188@gmail.com>
2018-05-07 22:02:00 +0800

    feat: Add lowercase
    Change all search result to lowercase.

--- py_01_search_text/daniel_search.py
+++ py_01_search_text/daniel_search.py
@@ -4,10 +4,10 @@
 
 Keyword = input("Type Keyword: ")
 k=0
-with open("py_01_search_text/article.txt","r") as f:
+with open("article.txt","r") as f:
 	for line in f:
 		words = line.split()
 		for i in words:
-			if (i ==Keyword):
+			if (i.lower() ==Keyword):
 				k=k+1
 	print("We have found", k, "matched in article")
