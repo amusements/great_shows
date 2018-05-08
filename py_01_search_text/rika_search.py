ae099b1897ded675e94bd16d0e1095fc2683c259
RikaLin <rikaya74@gmail.com>
2018-05-08 19:58:50 +0800

    feat: 調整為不區分大小寫搜尋

--- py_01_search_text/rika_search.py
+++ py_01_search_text/rika_search.py
@@ -4,10 +4,11 @@ file = open("article.txt", "r")
 fileContent = file.readlines()
 file.close()
 
+inputTextLower = inputText.lower()
 matchedCount = 0;
 for line in fileContent:
     for word in line.split():
-        if word.find(inputText) != -1:
-        	matchedCount += 1
+        wordLower = word.lower()
+        matchedCount += wordLower.count(inputTextLower)
 
 print("Keyword {}: We have found {} matched in article".format(inputText, matchedCount))
