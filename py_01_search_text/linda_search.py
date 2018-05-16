a8b36d1b3e546db5de9580eb7bdda6366fa466f5
Linda0924 <luckygirl0924@gmail.com>
2018-05-17 04:38:28 +0800

    feat: 搜尋字串要不分大小寫

--- py_01_search_text/linda_search.py
+++ py_01_search_text/linda_search.py
@@ -1,8 +1,5 @@
-# coding=UTF-8
-import os
-os.chdir('/Users/linda.hsu/dev/great_shows/py_01_search_text')
-
-word = input('Input your world: ').strip()
-with open('article.txt', 'r') as f:
-    #print(sum(line.count(word) for line in f))
-    print ("keyword",(word),":We have found",(sum(line.count(word) for line in f)),"matched in article")
+word = format(input('Input yout word: ')).lower()
+with open('article.txt','r') as f:
+    for line in f:
+        if word in line.lower():
+                print ("keyword",(word),":We have found",(sum(line.count(word) for line in f)),"matched in article")
