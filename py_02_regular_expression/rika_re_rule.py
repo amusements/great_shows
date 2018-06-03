3e89a920bdf3c654daf26575cf4046207d723fd3
RikaLin <rikaya74@gmail.com>
2018-06-03 11:08:22 +0800

    fix: 修正身分證字號錯誤

--- py_02_regular_expression/rika_re_rule.py
+++ py_02_regular_expression/rika_re_rule.py
@@ -13,22 +13,28 @@ idVerifyDictionary = {"A": 10, "B": 11, "C": 12,
                       "V": 29, "W": 32, "X": 30,
                       "Y": 31, "Z": 33}
 
+def verify_tw_id(tw_id):
+    strStrip = tw_id.strip()
+    strStripMaxIndex = len(strStrip)-1
+    strStripFirstWord = strStrip[0]
+    verifyKey = idVerifyDictionary[strStripFirstWord] 
+    verifyInt = int(strStrip[strStripMaxIndex])
+    verifyInt += int(verifyKey / 10) + (verifyKey % 10) * 9
+    i = 8;
+    for x in range(1, strStripMaxIndex):
+        verifyInt += int(strStrip[x]) * i
+        i -= 1
+    if verifyInt % 10 == 0:
+        return 1
+    else :
+        return 0
+
 if re.match(mailRegular, inputText) != None:
     print("Keyword {} is a email".format(inputText))
 elif re.match(phoneRegular, inputText) != None:
     print("Keyword {} is a mobile phone number".format(inputText))
 elif re.match(twIdRegular, inputText) != None:
-    strStrip = inputText.strip()
-    strStripMaxIndex = len(strStrip)-1
-    strStripFirstWord = strStrip[0]
-    verifyKey = idVerifyDictionary[strStripFirstWord] 
-    verifyInt = int(strStrip[strStripMaxIndex])
-    verifyInt += verifyKey / 10 + (verifyKey % 10) * 9
-    i = 8;
-    for x in range(1, strStripMaxIndex):
-        verifyInt += x * i
-        i -= 1
-    if verifyInt % 10 == 0:
+    if bool(verify_tw_id(inputText)):
         print("Keyword {} is a TW id".format(inputText))
     else :
         print("Keyword {} is not TW id".format(inputText))
