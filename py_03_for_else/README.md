524f6e79c4fda4869f2db7091d29d91866823897
yiwenhuang <login0807@gmail.com>
2018-06-18 13:22:20 +0800

    Three exercise to practice for-loop

---
+++ py_03_for_else/README.md
@@ -0,0 +1,6 @@
+[參考網站](https://docs.python.org/3/tutorial/controlflow.html#break-and-continue-statements-and-else-clauses-on-loops)
+
+1. Track & pull branch feature/while_else
+2. Copy sample_file.py under the folder py_03_while_else and named as [yourname]_re_rule.py
+3. Finish 3 excercises and match the expected output in file.
+4. Push/Submit your code to github
---
+++ py_03_for_else/sample.py
@@ -0,0 +1,19 @@
+big_box = ["Orchild", "Mickey Mouse", "Orange", "Cat", "Cow", "Lemon Tree", "Red Pepper"]
+
+
+# TODO: Using for-loop & continue to add water on plants, but not animal
+# OUTPUT:
+#   Orchild watered.
+#   Orange waterd.
+#   Lemon Tree watered.
+#   Red Pepper waterd.
+
+
+# TODO: Using for-loop & break to feed Cow only
+# OUTPUT:
+#   Cow feeded.
+
+
+# TODO: Using for-loop & else to wash a car
+# OUTPUT:
+#   No car to wash.
