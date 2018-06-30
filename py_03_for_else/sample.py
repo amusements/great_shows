d18d5fef29f36b5c8fcd6a1859d660ade3a38853
yiwenhuang <login0807@gmail.com>
2018-06-30 10:38:22 +0800

    Fix "Leon Tree" to "Lemon Tree"

--- py_03_for_else/sample.py
+++ py_03_for_else/sample.py
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-big_box = ["Orchild", "Mickey Mouse", "Orange", "Cat", "Cow", "Leon Tree", "Red Pepper"]
-=======
 big_box = ["Orchild", "Mickey Mouse", "Orange", "Cat", "Cow", "Lemon Tree", "Red Pepper"]
->>>>>>> 524f6e79c4fda4869f2db7091d29d91866823897
-
 
 # TODO: Using for-loop & continue to add water on plants, but not animal
 # OUTPUT:
 #   Orchild watered.
 #   Orange waterd.
-<<<<<<< HEAD
-#   Leon Tree watered.
-=======
 #   Lemon Tree watered.
->>>>>>> 524f6e79c4fda4869f2db7091d29d91866823897
 #   Red Pepper waterd.
 
 
