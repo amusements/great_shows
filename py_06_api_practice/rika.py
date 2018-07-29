c549cce51c476fca22fe7b9bbd379882dfb7a6d8
RikaLin <rikaya74@gmail.com>
2018-07-29 13:51:42 +0800

    feat: practice JSON API

---
+++ py_06_api_practice/rika.py
@@ -0,0 +1,19 @@
+import json
+import urllib.request, urllib.parse, urllib.error
+sourceURL = "https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=2"
+source = urllib.request.urlopen(sourceURL)
+decodeSource = source.read().decode()
+sourceJson = json.loads(decodeSource)
+dramaArr = []
+for objDict in sourceJson:
+	for infoDict in objDict["showInfo"]:
+		title = objDict["title"]
+		time = infoDict["time"]
+		location = infoDict["location"]
+		locationName = infoDict["locationName"]
+		dramaDic = {"title": title, "time": time, "location": location, "locationName": locationName}
+		dramaArr.append(dramaDic)
+		print("Drama Title: "+title)
+		print("Location: {0} ({1})".format(locationName, location))
+		print("Performance time: "+time)
+print("Total count: {0}".format(len(dramaArr)))
