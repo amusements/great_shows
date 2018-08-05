fb8ea394d856c92fc8de7eb73ecd838e572f7b9a
RikaLin <rikaya74@gmail.com>
2018-08-05 21:14:02 +0800

    fix: 增加表演時間判斷

--- py_06_api_practice/rika.py
+++ py_06_api_practice/rika.py
@@ -6,14 +6,15 @@ decodeSource = source.read().decode()
 sourceJson = json.loads(decodeSource)
 dramaArr = []
 for objDict in sourceJson:
-	for infoDict in objDict["showInfo"]:
-		title = objDict["title"]
-		time = infoDict["time"]
-		location = infoDict["location"]
-		locationName = infoDict["locationName"]
-		dramaDic = {"title": title, "time": time, "location": location, "locationName": locationName}
-		dramaArr.append(dramaDic)
-		print("Drama Title: "+title)
-		print("Location: {0} ({1})".format(locationName, location))
-		print("Performance time: "+time)
+    for infoDict in objDict["showInfo"]:
+        time = infoDict["time"]
+        if (time >= "2018/01/01 00:00:00"):
+            title = objDict["title"]
+            location = infoDict["location"]
+            locationName = infoDict["locationName"]
+            dramaDic = {"title": title, "time": time, "location": location, "locationName": locationName}
+            dramaArr.append(dramaDic)
+            print("Drama Title: "+title)
+            print("Location: {0} ({1})".format(locationName, location))
+            print("Performance time: "+time)
 print("Total count: {0}".format(len(dramaArr)))
