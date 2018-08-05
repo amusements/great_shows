2049803c8cf54dec016083c5c6bf622ae5fa1fa3
Wei Lin Lee <gtr8188@gmail.com>
2018-08-05 21:36:30 +0800

    Modified HW code
    Modify HW code

--- py_06_api_practice/daniel.py
+++ py_06_api_practice/daniel.py
@@ -1,32 +1,26 @@
 import json
 import urllib.request, urllib.parse, urllib.error
 
-
 url = 'https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=2'
-uh = urllib.request.urlopen(url)
-data = uh.read().decode()
-added = 0
-
+data = urllib.request.urlopen(url).read().decode()
+count = 0
 #print(data)
-
 try:
   js = json.loads(data)
 except:
   js = None
 
 #print(json.dumps(js, indent=4))
-
 for item in js:
-	title = item['title']
-	#showunit = item['showUnit']
-	showInfo = item['showInfo']
-	#time = item['showInfo'][0]['time']
-	#print(time)
-	for value in showInfo:
-		time = value['time']
-		location = value['location']
-		if time >= '2018/01/01 00:00:00':
+	for value in item['showInfo']:
+		if value['time'] >= '2018/01/01 00:00:00':
+			title = item['title']
+			time = value['time']
+			#showunit = item['showUnit']
+			#time = item['showInfo'][0]['time']
+			#print(time)			
+			location = value['location']
 			print('戲劇名稱:' ,title, '表演地點:',location, '表演時間', time)
-			added +=1
-print(added)
+			count +=1
+print(count)
 		
