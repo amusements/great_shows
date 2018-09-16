1f49b3c92343e48cc5a9b329b517e3fd06f9625e
Lily <yaliyang1214@gmail.com>
2018-09-16 19:56:38 +0800

    add filter:startdate, enddate, location search

--- py_06_api_practice/lily.py
+++ py_06_api_practice/lily.py
@@ -5,25 +5,34 @@ drama= urllib.request.urlopen('https://cloud.culture.tw/frontsite/trans/SearchSh
 drama_list= drama.read().decode()
 total_list=json.loads(drama_list) 
 
+start= input("Start date(0000/00/00 00:00:00):")
+end= input("End date(0000/00/00 00:00:00):")
+theater = input("Location:")
+
 count=0    
 for dramadict in total_list:
     for infodict in dramadict["showInfo"]:
-            title = dramadict["title"]
-            time = infodict["time"]
-            location = infodict["location"] 
-            locationName = infodict["locationName"]   
-            performancedict ={"title": title, "locationName": locationName, "location":location, "time": time}
-        if infodict["time"] >= "2018/01/01 00:00:00":
-            count +=1
+        title = dramadict["title"]
+        time = infodict["time"]
+        location = infodict["location"] 
+        locationName = infodict["locationName"]
+        price = infodict["price"]   
+#        if infodict["time"] >= "2018/01/01 00:00:00":
+#            count +=1
 
-    print ("Drama Title:" +title)
-    print ("Location: {0} ({1})".format(locationName, location))
-    print ("Preformance time:" +time)
-    print ("total count:" +count)
-
-
+#        print ("Drama Title:" ,title)
+#        print ("Location: {0} ({1})".format(locationName, location))
+#        print ("Preformance time:" ,time)
+#print ("total count:" ,count)
 
 
-
-
+        if (start <= infodict["time"] <= end) and (theater in infodict["locationName"]):
+            print ("Drama Title:", title)
+            print ("Performance time:", time)
+            print ("Location: {0} ({1})".format(locationName, location))
+            print ("Price:", price)        
+        else:
+            print ("no results")
+    
+    
 
