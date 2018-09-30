2fee943b345f16c26aa855ed4917a36fb8b336bc
Lily <yaliyang1214@gmail.com>
2018-09-30 21:18:09 +0800

    比對地區：但台臺還未處理，且若有地址會比對到

--- py_06_api_practice/lily.py
+++ py_06_api_practice/lily.py
@@ -1,15 +1,25 @@
 #!/Users/yaliyang/anaconda3/bin/python3
+import re
 import json
 import urllib.request, urllib.parse, urllib.error
 drama= urllib.request.urlopen('https://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=2 ')
 drama_list= drama.read().decode()
 total_list=json.loads(drama_list) 
 
+city = input("City/County:")
 start= input("Start date(0000/00/00 00:00:00):")
 end= input("End date(0000/00/00 00:00:00):")
-theater = input("Location:")
+theater = input("Theater:")
 
-count=0    
+if len(city) ==0:
+    print("You have to fill all the information needed!")
+elif len(start) ==0:
+    print("You have to fill all the information needed")
+elif len(end) ==0:
+    print("You have to fill all the information needed")
+elif len(theater) ==0:
+    print("You have to fill all the information needed")
+
 for dramadict in total_list:
     for infodict in dramadict["showInfo"]:
         title = dramadict["title"]
@@ -17,22 +27,12 @@ for dramadict in total_list:
         location = infodict["location"] 
         locationName = infodict["locationName"]
         price = infodict["price"]   
-#        if infodict["time"] >= "2018/01/01 00:00:00":
-#            count +=1
+#        recity = re.sub("台", "臺", str(infodict["location"]))
+        if (city in infodict["location"]) and (start <= infodict["time"] <= end) and (theater in infodict["locationName"]):
+                print ("Drama Title:", title)
+                print ("Performance time:", time)
+                print ("Theater: {0} ({1})".format(locationName, location))
+                print ("Price:", price)           
+        
 
-#        print ("Drama Title:" ,title)
-#        print ("Location: {0} ({1})".format(locationName, location))
-#        print ("Preformance time:" ,time)
-#print ("total count:" ,count)
-
-
-        if (start <= infodict["time"] <= end) and (theater in infodict["locationName"]):
-            print ("Drama Title:", title)
-            print ("Performance time:", time)
-            print ("Location: {0} ({1})".format(locationName, location))
-            print ("Price:", price)        
-        else:
-            print ("no results")
-    
-    
 
