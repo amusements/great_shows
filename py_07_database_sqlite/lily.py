51bba15db95750dc763212e60d85bbc30f07be15
Lily <yaliyang1214@gmail.com>
2018-11-12 21:22:54 +0800

    re-wrote practice

--- py_07_database_sqlite/lily.py
+++ py_07_database_sqlite/lily.py
@@ -11,22 +11,22 @@ drama= urllib.request.urlopen('https://cloud.culture.tw/frontsite/trans/SearchSh
 drama_list= drama.read().decode()
 total_list=json.loads(drama_list)
 
-for info in total_list:
-    UID = info ["UID"]
-    title = info["title"]
-    category = info["category"] 
-
 con = lite.connect('lily.db')
- 
-with con:
- 
-    cur = con.cursor()    
-   # cur.execute("CREATE TABLE DRAMA (UID INT, title TEXT, category TEXT)")   
+cur = con.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS DRAMA (UID INT, title TEXT, category TEXT)")
 
-    cur.execute("INSERT INTO DRAMA VALUES ('"+UID+"', '"+title+"', '"+category+"')")
+for info in total_list:
+    UID=info["UID"]
+    title=info["title"]
+    category=info["category"] 
+    cur.execute("INSERT INTO DRAMA VALUES(?, ?, ?)", [info["UID"], info["title"], info["category"]]) 
+    con.commit()
 
+with con: 
     cur.execute("SELECT COUNT(*) FROM DRAMA")
- 
-    rows_count = cur.fetchall()
+    rows = cur.fetchall()
+    print(rows)
 
-    print (rows_count)
+
+
+
